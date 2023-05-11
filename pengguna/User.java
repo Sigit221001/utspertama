21f283a4b98bcdfacb0f936739ce41d144b69456
Mohamad Sigit <mohamadsigitriyanto@gmail.com>
2023-05-11 17:00:19 +0700

    fix coding UTS PBO

--- pengguna/User.java
+++ pengguna/User.java
@@ -10,10 +10,22 @@ public class User {
     }
 
     public String getUsername() {
-        return this.username;
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
-        return this.password;
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
     }
 }
