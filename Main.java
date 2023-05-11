fc781f17f884c05631c37ce4ebc0cce0e745a060
Mohamad Sigit <mohamadsigitriyanto@gmail.com>
2023-05-11 16:25:43 +0700

    fix coding UTS PBO

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ class Main {
 
         Mhs mhs = new Mhs("Ini_Username", "Ini_Password", "Mohamad Sigit Riyanto", "G.111.19.0011", 8);
 
-        mhs.setNama("Mohamad Sigit Riyanto");
-        mhs.setNim("G.111.19.0011");
-        mhs.setSmt(8);
+        // mhs.setNama("Mohamad Sigit Riyanto");
+        // mhs.setNim("G.111.19.0011");
+        // mhs.setSmt(8);
 
         System.out.println("Nama Mhs : " + mhs.getNama());
         System.out.println("Nim Mhs : " + mhs.getNim());
